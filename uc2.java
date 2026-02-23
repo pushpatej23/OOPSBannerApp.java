28badd112e72e35215e5958b80e5f84ff271a9be
d.pushpatej <your.email@example.com>
2026-02-23 15:40:01 +0530

    first commit

---
+++ uc2.java
@@ -0,0 +1,33 @@
+/**
+ * OOPSBannerApp UC2 - Print Banner
+ * This version prints the OOPS banner using ASCII art.
+ * Each letter is designed to be ~9 characters wide and 7 lines high.
+ *
+ * @author Developer
+ * @version 2.1
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println("  *****  " + "  " + "  *****  " + "  " + " ******  " + "  " + "  *****  ");
+
+        // Line 2
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *     * " + "  " + " *     * ");
+
+        // Line 3
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *     * " + "  " + " *       ");
+
+        // Line 4
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " ******  " + "  " + "  *****  ");
+
+        // Line 5
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *       " + "  " + "       * ");
+
+        // Line 6
+        System.out.println(" *     * " + "  " + " *     * " + "  " + " *       " + "  " + " *     * ");
+
+        // Line 7
+        System.out.println("  *****  " + "  " + "  *****  " + "  " + " *       " + "  " + "  *****  ");
+    }
+}
